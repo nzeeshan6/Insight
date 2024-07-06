1337fd69977643e9792dbff99b166a237c0a9b9d
Zeeshan Naseem <57848064+nzeeshan6@users.noreply.github.com>
2024-07-06 01:15:07 +0000

    AI Integration Done with Final Checks

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
+// Importing necessary libraries
 import express from "express";
-import json from "body-parser";
+import bodyParser from "body-parser";
 import serveFavicon from "serve-favicon";
 import path from "path";
-// import Server from "http";
+import { callGPT4 } from "./gptFlow.js";
 
+// Creating constants and Global Variables
 const app = express();
 const dirname = import.meta.dirname;
-// const server = Server(app);
+let result = {};
+let loadingMessage = "Insights are being generated. Please Wait ...";
 
+// Additional Features to Express App
 app.use(express.static(path.join(dirname + "/public")));
 app.use(serveFavicon(dirname + "/public/favicon/favicon-16x16.png"));
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log("Listening at PORT [3000]");
 });
 
+// Landing Page Route
 app.get("/", (request, response) => {
   response.sendFile(path.join(dirname + "/public/src/index.html"));
+});
+
+// First POST Data Route
+app.post("/insights", async (request, response) => {
+  const patentNumber = request.body.patentNumber;
+  const searchLogs = request.body.searchLogs;
+  // console.log("Request: ", request);
+  // console.log("Patent Number received: ", patentNumber);
+  // console.log("Search Log received: ", searchLogs);
+  result = {};
+  loadingMessage = "Insight is being generated. Please Wait ...";
+  response.setHeader("202", { "content-type": "application/json" });
+  // response.send({ message: "All things are received okay" });
+  // response.end();
+  response.status(202).json({ message: loadingMessage });
+
+  // Simulate a long-running process
+  // setTimeout(() => {
+  //   result = { message: `Processed insights for patent ${patentNumber}` };
+  // }, 1000); // Adjust the delay as needed
+  // result = {message: await callGPT4(patentNumber, searchLogs)};
+  const processedResponse = await callGPT4(patentNumber, searchLogs);
+  result = { message: processedResponse };
+});
+
+// Refresh Route
+app.get("/insight-result", (request, response) => {
+  // console.log("Request Recevied at Insight Result");
+  if (result.message) {
+    response.setHeader("200", { "content-type": "application/json" });
+    response.status(200).json(result);
+  } else {
+    loadingMessage += ".";
+    response.setHeader("202", { "content-type": "application/json" });
+    response.status(202).json({ message: loadingMessage });
+  }
 });
---
+++ public/script/index.js
@@ -0,0 +1,62 @@
+let patentNumberField = document.getElementById("patentNumber");
+let searchLogsField = document.getElementById("searchLogs");
+let generateButton = document.getElementById("getInsight");
+let insightField = document.getElementById("insightField");
+
+generateButton.addEventListener("click", async () => {
+  var patentNumber = patentNumberField.value;
+  var searchLogs = searchLogsField.value;
+
+  // Check if Everything is Correctly Filled
+  if (
+    patentNumber &&
+    searchLogs &&
+    patentNumber.length > 0 &&
+    searchLogs.length > 0
+  ) {
+    // Deisplaying the Response Section
+    let responseSection = document.getElementById("response-section");
+    responseSection.style = "display:block";
+    responseSection.scrollIntoView({ behaviour: "smooth" });
+    try {
+      // Pushing the input data to the Server
+      const response = await fetch("/insights", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          patentNumber: patentNumber,
+          searchLogs: searchLogs,
+        }),
+      });
+      // Awaiting Response for the first push
+      const text = await response.text();
+      const data = await JSON.parse(text);
+
+      // Updating the Status
+      insightField.innerHTML = data.message;
+      // console.log(response.status);
+
+      // Display the data on your web page or handle it as needed
+      const refreshInterval = setInterval(async () => {
+        const generatedInsightResponse = await fetch("/insight-result", {
+          method: "GET",
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+        const resultData = await generatedInsightResponse.text();
+        const generatedInsight = await JSON.parse(resultData);
+        // console.log("Generated Insight: ", generatedInsight);
+        if (generatedInsightResponse.status === 200) {
+          clearInterval(refreshInterval);
+        }
+        insightField.innerHTML = generatedInsight.message;
+      }, 2000);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  } else {
+  }
+});
